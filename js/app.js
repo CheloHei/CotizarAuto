38ae2818c71b975231e2432e83864f4275f23289
Marcelo Heinichen <romarhegu@gmail.com>
2019-06-14 14:59:12 -0400

    "Modificado archivo de proto ecmas6

--- js/app.js
+++ js/app.js
@@ -1,101 +1,103 @@
 
-function Seguro(marca, anio, tipo){
+class Seguro{
+    constructor(marca,anio,tipo){
     this.marca = marca;
     this.anio = anio;
     this.tipo = tipo;
-
-}
-Seguro.prototype.cotizarSeguro = function(){
-    //console.log(this.marca);
-    let cantidad;
-    const base = 2000;
-
-    switch (this.marca) {
-        case '1':
-            cantidad = base * 1.15;
-            break;
-        case '2':
-            cantidad = base * 1.05;
-            break;
-        case '3':
-            cantidad = base * 1.35;
-            break;
     }
 
-    //lectura de año
-    const diferencia = new Date().getFullYear() - this.anio; 
-    cantidad -= ((diferencia * 0.03 ) * cantidad)
-    if (this.tipo === 'basico') {
-        cantidad *= 1.30;
-    }else{
-        cantidad *= 1.50;
+    cotizarSeguro(){
+        //console.log(this.marca);
+        let cantidad;
+        const base = 2000;
+    
+        switch (this.marca) {
+            case '1':
+                cantidad = base * 1.15;
+                break;
+            case '2':
+                cantidad = base * 1.05;
+                break;
+            case '3':
+                cantidad = base * 1.35;
+                break;
+        }
+    
+        //lectura de año
+        const diferencia = new Date().getFullYear() - this.anio; 
+        cantidad -= ((diferencia * 0.03 ) * cantidad)
+        if (this.tipo === 'basico') {
+            cantidad *= 1.30;
+        }else{
+            cantidad *= 1.50;
+        }
+        //console.log(cantidad);
+        //por cada año menos 3%
+        //console.log(diferencia);
+        return cantidad;
     }
-    //console.log(cantidad);
-    //por cada año menos 3%
-    //console.log(diferencia);
-    return cantidad;
-}
-
-function Interfaz(){}
-
-
-
-Interfaz.prototype.mostrarError = (mensaje,tipo)=>{
-    const div = document.createElement('div');
-
-    if(tipo === 'error') {
-        div.classList.add('mensaje','error');
-    }else{
-        div.classList.add('mensaje','correcto');
-    }
-    div.innerHTML = `${mensaje}`;
-    formulario.insertBefore(div,document.querySelector('.form-group'));
-
-    setTimeout(()=>{
-        document.querySelector('.mensaje').remove();
-    },3000);
 }
 
-//imprime resultado de cotizacion
-Interfaz.prototype.mostrarResultado = function(seguro,total){
-    const resultado = document.getElementById('resultado');
-    let marca;
-    switch (seguro.marca) {
-        case '1':
-        marca = 'Americano';
-            break;
-        case '2':
-        marca = 'Asiatico';
-            break;
-        case '3':
-        marca = 'Europeo';
-            break;
+class Interfaz{
+
+    mostrarError = (mensaje,tipo)=>{
+
+        const div = document.createElement('div');
     
-        default:
-            break;
+        if(tipo === 'error') {
+            div.classList.add('mensaje','error');
+        }else{
+            div.classList.add('mensaje','correcto');
+        }
+        div.innerHTML = `${mensaje}`;
+        formulario.insertBefore(div,document.querySelector('.form-group'));
+    
+        setTimeout(()=>{
+            document.querySelector('.mensaje').remove();
+        },3000);
     }
-
-    const div = document.createElement('div');
-
-    div.innerHTML = `
-                    <p class='header'>Tu Resumen:</p>
-                    <p>Marca ${marca}</p>
-                    <p>Año: ${seguro.anio}</p>
-                    <p>Tipo: ${seguro.tipo}</p>
-                    <p>Total: $ ${total}</p>
-    
-    
-    `;
-    const spinner = document.querySelector('#cargando img');
-    spinner.style.display='block';
-    setTimeout(()=>{
+    //imprime resultado de cotizacion
+    mostrarResultado = function(seguro,total){
+        const resultado = document.getElementById('resultado');
+        let marca;
+        switch (seguro.marca) {
+            case '1':
+            marca = 'Americano';
+                break;
+            case '2':
+            marca = 'Asiatico';
+                break;
+            case '3':
+            marca = 'Europeo';
+                break;
         
-        spinner.style.display='none';
-        resultado.appendChild(div);
-    },2000);
-
-    console.log(marca);
+            default:
+                break;
+        }
+    
+        const div = document.createElement('div');
+    
+        div.innerHTML = `
+                        <p class='header'>Tu Resumen:</p>
+                        <p>Marca ${marca}</p>
+                        <p>Año: ${seguro.anio}</p>
+                        <p>Tipo: ${seguro.tipo}</p>
+                        <p>Total: $ ${total}</p>
+        
+        
+        `;
+        const spinner = document.querySelector('#cargando img');
+        spinner.style.display='block';
+        setTimeout(()=>{
+            
+            spinner.style.display='none';
+            resultado.appendChild(div);
+        },2000);
+    
+        console.log(marca);
+    }
 }
+
 
 
 //EventListeners
